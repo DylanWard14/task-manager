13a9299836f56c6d4b5252361dc3ea588a956207
DylanWard14 <dylan.ward@iinet.net.au>
2019-04-04 15:00:17 +1000

    Added function to filter tasks by their completed status

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ const main = async () => {
     // await task.populate('owner').execPopulate();
     // console.log(task.owner);
 
-    const user = await User.findById('5ca55da870df030d806a28c9');
-    await user.populate('tasks').execPopulate();
-    console.log(user.tasks);
+    // const user = await User.findById('5ca55da870df030d806a28c9');
+    // await user.populate('tasks').execPopulate();
+    // console.log(user.tasks);
 }
 
 main();
